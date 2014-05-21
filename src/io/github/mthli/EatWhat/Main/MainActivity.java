6fca22bd40d41168158ac8e97b0795d76438a562
newmlee <matthewlee0725@gmail.com>
2014-05-21 22:08:04 +0000

    更新

--- src/io/github/mthli/EatWhat/Main/MainActivity.java
+++ src/io/github/mthli/EatWhat/Main/MainActivity.java
@@ -1,24 +1,76 @@
 package io.github.mthli.EatWhat.Main;
 
 import android.app.Activity;
+import android.app.Service;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 import io.github.mthli.EatWhat.About.AboutActivity;
 import io.github.mthli.EatWhat.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+    private ImageView background;
+    private SensorManager sensorManager;
+    private Vibrator vibrator;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        ImageView background = (ImageView) findViewById(R.id.main_background_image);
+        background = (ImageView) findViewById(R.id.main_background_image);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(
+                this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL
+        );
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Do nothing */
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int sensorType = sensorEvent.sensor.getType();
+        float[] values= sensorEvent.values;
+
+        if (sensorType == Sensor.TYPE_ACCELEROMETER) {
+            if ((Math.abs(values[0]) > 17) || Math.abs(values[1]) > 17 || Math.abs(values[2]) > 17){
+                Toast.makeText(
+                        MainActivity.this,
+                        "sjahdisygdsaig",
+                        Toast.LENGTH_SHORT
+                ).show();
+                vibrator.vibrate(500);
+            }
+        }
     }
 
     @Override
@@ -31,6 +83,9 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
         switch (menuItem.getItemId()) {
+            case R.id.main_menu_share:
+                /* Do something */
+                break;
             case R.id.main_menu_about:
                 Intent intent_about = new Intent(MainActivity.this, AboutActivity.class);
                 startActivity(intent_about);

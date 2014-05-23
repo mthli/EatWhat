28f5f3130a71dc8da575d62e5da941e1b721aea8
newmlee <matthewlee0725@gmail.com>
2014-05-23 09:24:06 +0000

    完成自定义背景选项

--- src/io/github/mthli/EatWhat/Main/MainActivity.java
+++ src/io/github/mthli/EatWhat/Main/MainActivity.java
@@ -3,6 +3,7 @@ package io.github.mthli.EatWhat.Main;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Service;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
@@ -13,15 +14,20 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.provider.MediaStore;
 import android.view.*;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 
+import android.widget.Toast;
 import io.github.mthli.EatWhat.About.AboutActivity;
 import io.github.mthli.EatWhat.R;
+
+import java.io.FileNotFoundException;
 
 public class MainActivity extends Activity implements SensorEventListener, ActionBar.OnNavigationListener {
     private ImageView background;
@@ -59,11 +65,12 @@ public class MainActivity extends Activity implements SensorEventListener, Actio
 
         sharedPreferences = getSharedPreferences("setting", MODE_PRIVATE);
         editor = sharedPreferences.edit();
-        String path = sharedPreferences.getString("background", null);
+        
+        String path = sharedPreferences.getString("bitmap_background", null);
         background = (ImageView) findViewById(R.id.main_background_image);
         if (path != null) {
-            bitmap = BitmapFactory.decodeFile(path);
-            background.setImageBitmap(bitmap);
+            Uri uri = Uri.parse(path);
+            background.setImageURI(uri);
         } else {
             background.setImageResource(R.drawable.main_background);
         }
@@ -138,12 +145,16 @@ public class MainActivity extends Activity implements SensorEventListener, Actio
                  */
                 Intent intent_share = new Intent(Intent.ACTION_SEND);
                 intent_share.setType("text/plain");
-                intent_share.putExtra(Intent.EXTRA_SUBJECT, "Share");
+                intent_share.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.share_title));
+                /* Do something */
                 intent_share.putExtra(Intent.EXTRA_TEXT, "dhusga");
                 startActivity(Intent.createChooser(intent_share, "Share"));
                 break;
             case R.id.main_menu_background:
-                /* Do something */
+                Intent intent_background = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent_background.setType("image/*");
+                intent_background.putExtra("return-data", true);
+                startActivityForResult(intent_background, 1);
                 break;
             case R.id.main_menu_usual:
                 /* Do something */
@@ -156,5 +167,27 @@ public class MainActivity extends Activity implements SensorEventListener, Actio
                 break;
         }
         return super.onOptionsItemSelected(menuItem);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 1 && data != null) {
+            Uri uri = data.getData();
+            System.out.println(uri.getPath());
+            ContentResolver contentResolver = this.getContentResolver();
+            try {
+                bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(uri));
+                background.setImageBitmap(bitmap);
+                editor.putString("bitmap_background", uri.toString());
+                editor.commit();
+            } catch (FileNotFoundException f) {
+                Toast.makeText(
+                        MainActivity.this,
+                        getString(R.string.error_background_404),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        }
     }
 }
